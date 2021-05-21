a434c5b3194ff47d29e10a75e4efe82812741b9e
Omair A. Mangondaya <67724946+omairalamada@users.noreply.github.com>
2021-05-21 08:51:58 +0800

    initial commit

---
+++ src/main/java/com/voicecaster/voicecaster/VoiceCasterApplication.java
@@ -0,0 +1,13 @@
+package com.voicecaster.voicecaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceCasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceCasterApplication.class, args);
+	}
+
+}
