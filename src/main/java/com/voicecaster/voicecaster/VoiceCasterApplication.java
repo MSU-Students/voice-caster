4a36860aa5bd048dd92b5bdd255c2bcac8d9656f
ruzlicali16 <ruzlicali24@gmail.com>
2021-07-09 09:40:23 +0800

    main-app + voice controller + web socket config

--- src/main/java/com/voicecaster/voicecaster/VoiceCasterApplication.java
+++ src/main/java/com/voicecaster/voicecaster/VoiceCasterApplication.java
@@ -2,12 +2,31 @@ package com.voicecaster.voicecaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class VoiceCasterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VoiceCasterApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(VoiceCasterApplication.class, args);
+  }
 
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+          .addMapping("/**")
+		  .allowedOriginPatterns("*")
+          .allowCredentials(true);
+      }
+    };
+  }
 }
