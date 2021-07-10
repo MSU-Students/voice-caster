2debeca0ddd4bba14907111855361d3c58456d05
omairalamada <omairmangondaya@gmail.com>
2021-07-10 11:24:53 +0800

    updates

--- src/main/java/com/voicecaster/voicecaster/VoiceCasterApplication.java
+++ src/main/java/com/voicecaster/voicecaster/VoiceCasterApplication.java
@@ -2,6 +2,8 @@ package com.voicecaster.voicecaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,10 +13,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
-public class VoiceCasterApplication {
+public class VoiceCasterApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(VoiceCasterApplication.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(
+    SpringApplicationBuilder builder
+  ) {
+    return builder.sources(VoiceCasterApplication.class);
   }
 
   @Bean
@@ -24,7 +33,7 @@ public class VoiceCasterApplication {
       public void addCorsMappings(CorsRegistry registry) {
         registry
           .addMapping("/**")
-		  .allowedOriginPatterns("*")
+          .allowedOriginPatterns("*")
           .allowCredentials(true);
       }
     };
